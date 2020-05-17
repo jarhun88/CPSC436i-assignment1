496913031a5b83853c3959a62cdbb6a3b1a8d5ef
John <likisongproduct@gmail.com>
2020-05-16 18:30:13 -0700

    added close list item feature

--- app.js
+++ app.js
@@ -1,32 +1,58 @@
-function addMessage() {
-     let text = document.getElementById("text-input").value;
-     console.log(text)
-     if (text === "") 
-      return
-
-     let li = document.createElement("li")
-     li.className = 'msg';
-     li.textContent = text;
-     document.getElementById("list").appendChild(li);
-  }
-
-  function clearMessage() {
-    document.getElementById("text-input").value = "";
-  }
-
-  function clearList() {
-    let ul = document.getElementById("list");
-    while(ul.firstChild) ul.removeChild(ul.firstChild);
-  }
-
-
-  function loadDefaultMessages() {
-    let defaultMessages = ["apple", "banana", "carrot"];
-    for (word of defaultMessages) {
-      console.log(word)
-      let li = document.createElement("li")
-      li.className = 'msg';
-      li.textContent = word;
-      document.getElementById("list").appendChild(li)
+// Add a "checked" symbol when clicking on a list item
+function closeListItem() {
+  let close = document.getElementsByClassName("close");
+  let i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      let div = this.parentElement;
+      div.style.display = "none";
     }
   }
+}
+
+function addMessage() {
+  let text = document.getElementById("text-input").value;
+  console.log(text)
+  if (text === "") 
+    return
+
+  let li = document.createElement("li")
+  li.className = 'msg';
+  li.textContent = text;
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  document.getElementById("list").appendChild(li).appendChild(span)
+}
+
+function clearMessage() {
+  document.getElementById("text-input").value = "";
+}
+
+function clearList() {
+  let ul = document.getElementById("list");
+  while(ul.firstChild) ul.removeChild(ul.firstChild);
+}
+
+function loadDefaultMessages() {
+  let defaultMessages = ["apple", "banana", "carrot"];
+  for (word of defaultMessages) {
+    console.log(word)
+    let li = document.createElement("li")
+    li.className = 'msg';
+    li.textContent = word;
+
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.addEventListener("click", function() {
+      let ul = document.getElementById("list");
+      ul.removeChild(this.parentElement)
+    });
+    span.appendChild(txt);
+    document.getElementById("list").appendChild(li).appendChild(span)
+    
+  }
+}
+
